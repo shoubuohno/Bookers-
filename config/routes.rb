33ec7b27d2bbeaf925f4d4e02e7b6ef7c6efdf9c
ohnoshobu <ohno56sent@gmail.com>
2020-07-25 13:43:18 +0900

    model rename List_to_Book

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'homes#top'
+  get 'books' => 'homes#index'
+  post 'books' => 'homes#create'
+  get 'books/:id' => 'homes#show' ,as: 'book'
+  get 'books/:id/edit' => 'homes#edit', as: 'edit_book'
+  patch 'books/:id' => 'homes#update', as: 'update_book'
+  delete 'books/:id' => 'homes#destroy', as: 'destroy_book'
+end
